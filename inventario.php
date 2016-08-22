5858d97b03f36346c4c46284f8af778256f118d4
marck0793 <marck0793@hotmail.com>
2016-08-21 18:26:22 -0600

    Se crea la interfaz de inventario, con la tabla y la inclusión del header

---
+++ inventario.php
@@ -0,0 +1,20 @@
+<?php 
+// se incluye el header
+include_once 'header.php';
+?>
+
+<!-- Tabla de productos -->
+<table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Código</th>
+        <th>Descripción</th>
+        <th>Precio</th>
+        <th>Existencias</th>
+      </tr>
+    </thead>
+    <tbody>
+      
+    </tbody>
+</table>
+ 
